35a49953437f85f27fee1559a15fb29cecc14bfd
spewite <jiz.j12.a4@gmail.com>
2025-02-13 20:22:00 +0100

    Implement simple file upload POST to flask API.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+"use client"
+
+import { ChangeEvent } from "react";
 
 export default function Home() {
+
+  const handleFileUpload = async (event: ChangeEvent) => {
+    
+    const input = event.target as HTMLInputElement;
+  
+    if (!input.files || input.files.length===0) {
+      throw new Error("You must provide at least one image");
+    }
+
+    const file = input.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+  
+    const response = await fetch("http://127.0.0.1:5000/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+  
+    const result = await response.json();
+    console.log(result);
+  };
+  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex items-center justify-center min-h-screen bg-slate-950">
+      <input type="file" onChange={(e) => handleFileUpload(e)} />
     </div>
   );
 }
